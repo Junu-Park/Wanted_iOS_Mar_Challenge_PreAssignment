4c9273a8c862613339fe203eea15a62b5a4704fc
jwpark <jwpark@onesoftdigm.com>
2023-02-23 18:13:02 +0900

    프로젝트 및 기본 컴포넌트들 생성

---
+++ Mar_Challenge_PA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wanted_freeOnBoarding_iOS
+//
+//  Created by Dev OSD on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
