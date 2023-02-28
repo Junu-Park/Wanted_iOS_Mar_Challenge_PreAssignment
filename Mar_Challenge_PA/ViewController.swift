f8a672c244ae95f49910a22b71527070b4db334a
jwpark <jwpark@onesoftdigm.com>
2023-02-28 17:21:27 +0900

    1번 이미지뷰 이미지 로드 기능 적용

--- Mar_Challenge_PA/ViewController.swift
+++ Mar_Challenge_PA/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func LoadimgView1(_ sender: UIButton) {
+        imgView1.image = UIImage(systemName: "photo")
+        let url = URL(string: "https://developer.apple.com/swift/images/swift-og.png")
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url!) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self!.imgView1.image = image
+                    }
+                }
+            }
+        }
+    }
+    
 }
 
